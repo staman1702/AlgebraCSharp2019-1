a9e32afc1035ba2de1c16dae060ff612173d9485
staman1702 <staman1702@gmail.com>
2019-02-21 18:55:32 +0100

    4.1 Uvod

--- ConsoleApp1/Zbroj/Program.cs
+++ ConsoleApp1/Zbroj/Program.cs
@@ -10,6 +10,20 @@ namespace Zbroj
     {
         static void Main(string[] args)
         {
+            // deklaracija varijabli
+            int x, y;
+
+            // definicjia varijabli x i y
+            x = 5;
+            y = x + 3;
+
+            // Ispis u konzulu
+            Console.WriteLine("broj x je: " + x);
+            Console.WriteLine("broj y je: " + y);
+            Console.WriteLine("njihov zbroj je: " + (x + y));
+
+            // Program ostaje otvoren dok ne ukucamo broj/key
+            Console.ReadKey();
         }
     }
 }
