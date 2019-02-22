669a054c3c9758db0e03e86dabebef25028f6193
staman1702 <staman1702@gmail.com>
2019-02-22 16:15:01 +0100

    prva3zadatka

---
+++ ConsoleApp1/Ispis_brojeva/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispis_brojeva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            x = 1;
+            y = 2;
+            z = 3;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Ispis_ledista_i_vrelista_vode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispis_ledista_i_vrelista_vode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 0;
+            y = 100;
+            Console.WriteLine("Ledište vode je " + x + " stupnjeva Celsiusa.");
+            Console.WriteLine("Vrelište vode je " + y + " stupnjeva Celsiusa.");
+            Console.ReadKey();
+                      
+        }
+    }
+}
