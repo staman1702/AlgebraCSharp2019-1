d50a61e5ea4604216e7b9ea040a5a01a5fdd4c1d
staman1702 <staman1702@gmail.com>
2019-02-22 18:50:22 +0100

    vjezbenica_mrljanje

--- ConsoleApp1/Zbroj_razlika/Program.cs
+++ ConsoleApp1/Zbroj_razlika/Program.cs
@@ -5,13 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Zbroj_razlika
-{
+{       //comment: dodali smo varijablu "a" izvan main-a... ona je sada vani i ne sudjeluje u izvrsavanju programa.
+        //kako je dodati u main? NEMOJMO TO RADITI, NE ZNAM KAKO IZVEST TJ dodamo nakon Maina slijedece:
+    // .... Program xx = new Program();
     class Program
     {
+        private int a = 5559;
+
         static void Main(string[] args)
         {
             int a = 0, b = 0;
-            Console.WriteLine("Unesite 1. broj: ");
+            Console.WriteLine(@"Unesite 1. broj:\n.\no i\n\t\t ..\t. ");
             a = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Unesite 2. broj: ");
@@ -21,11 +25,12 @@ namespace Zbroj_razlika
 
             Console.WriteLine("Razlika je: {0}", a - b);
 
-            Console.WriteLine("Zbroj je: {0}, razlika je: {1}", a + b, a - b);
+            Console.WriteLine("Zbroj je: {0}, razlika je: {1}", a + b, a - b); //nula prva radnje, jedinica druga etc
 
-            Console.WriteLine("Umnožak je: {0}, kvocijent je: {1}", a * b, (decimal)a / (decimal)b);
+            Console.WriteLine("Umnožak je: {0}, kvocijent je: {1}", a * b, (decimal)a / (decimal)b); //moze i float!
 
             Console.WriteLine("Pritisnite tipku za izlaz...");
+
 
             Console.ReadLine();
 
