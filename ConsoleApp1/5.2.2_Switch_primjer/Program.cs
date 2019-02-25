52269acede9a33aeb9f25092d7ccf876a8dca6a4
staman1702 <staman1702@gmail.com>
2019-02-25 17:22:06 +0100

    switch uradak

---
+++ ConsoleApp1/5.2.2_Switch_primjer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._2_Switch_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0, b = 0;
+            string operacija = "";
+
+            Console.WriteLine("Molimo unesite dva prirodna broja i operaciju");
+            Console.WriteLine("1. broj: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. broj: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Operacija: ");
+            operacija = Console.ReadLine();
+
+
+            switch (operacija)
+            {
+                case "+":
+                case "plus":
+                    Console.WriteLine("zbroj je: {0} + {1} = {2}", a, b, (a + b));
+                    break;
+
+                case "-":
+                    Console.WriteLine("razlika je: {0} - {1} = {2}", a, b, (a - b));
+                    break;
+
+                case "*":
+                case "x":
+                
+                    Console.WriteLine("umnožak je: {0} * {1} = {2}", a, b, (a * b));
+                    break;
+
+                default:
+                    Console.WriteLine("kvocijent je: {0} / {1} = {2}", a, b, (a / b));
+                    break;
+
+            }
+            Console.WriteLine("\n\nZa izlaz pritisnite bilo koju tipku...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/5.2.3_exception/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._3_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
