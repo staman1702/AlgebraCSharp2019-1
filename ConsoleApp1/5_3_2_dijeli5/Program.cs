0c5b76931b3e7da1c4567c839ae4e554cc76f5c5
staman1702 <staman1702@gmail.com>
2019-02-26 18:24:01 +0100

    dijeli5do100

---
+++ ConsoleApp1/5_3_2_dijeli5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_2_dijeli5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 101; i = i + 5)
+
+            {
+                if (i == 0)
+                    {
+                    continue;
+                        }
+                Console.Write("{0}, ", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
