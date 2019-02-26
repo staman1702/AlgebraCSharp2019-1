08ab0ed5a5a5775ec11bf9a37e9b8c648a878bef
staman1702 <staman1702@gmail.com>
2019-02-26 18:36:57 +0100

    end of 26 feb

--- ConsoleApp1/5_3_1_neparni_do_20/Program.cs
+++ ConsoleApp1/5_3_1_neparni_do_20/Program.cs
@@ -7,15 +7,31 @@ using System.Threading.Tasks;
 namespace _5_3_1_neparni_do_20
 {
     class Program
-    {
+    {//MOJ PRIMJER:
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 1; i < 21; i= i + 2)
+
+        //    {
+        //        Console.Write("{0}, ", i);
+        //    }
+        //    Console.ReadLine();
+        //}
+
+
+            //PO KNJIZI
         static void Main(string[] args)
         {
-            for (int i = 1; i < 21; i= i + 2)
-
+            for (int i = 2; i <= 20; i++)
             {
-                Console.Write("{0}, ", i);
+                if (i % 2 != 0)
+
+                {
+                    Console.Write("{0}, ", i);
+                }
             }
             Console.ReadLine();
         }
     }
+
 }
---
+++ ConsoleApp1/5_3_3_suma100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_3_suma100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma prvih 100 brojeva");
+            int suma = 0;
+
+            for (int i = 0; i <= 5; i++)
+            {
+                suma += i;
+
+                Console.Write("{0}, ", suma);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
