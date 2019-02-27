864e8f5271b9aa43db54cd3ab971994b5d4288ec
staman1702 <staman1702@gmail.com>
2019-02-27 18:36:26 +0100

    27_02_odlazak

---
+++ ConsoleApp1/5_3_15_Ispis_unatrag/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_15_Ispis_unatrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.Write("Interval [1,10] unatrag: \n");
+
+                for (int i = 10; i >= 1; i--)
+                {
+                    
+                        Console.WriteLine("{0} ", i);
+                   
+                }
+                Console.ReadLine();
+           
+
+        }
+    }
+}
