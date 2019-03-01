c97fad807444a60dba5f6264ffc4c83cb37017b0
staman1702 <staman1702@gmail.com>
2019-03-01 15:43:59 +0100

    petak2

---
+++ ConsoleApp1/5_3_19_suma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_19_suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Unesite prirodne brojeve koje želite zbrojiti... \n \n(za izračun pritisnite nulu (0).");
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
