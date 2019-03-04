c1a0dc86a70b4ded0e919a9401dec329aade9821
staman1702 <staman1702@gmail.com>
2019-03-04 18:54:37 +0100

    04_03_2019

---
+++ ConsoleApp1/6_1_1_nizovi_prirucnik/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_1_1_nizovi_prirucnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //deklaracija u memoriji
+                int?[] niz = new int?[3];
+
+                //init
+                niz[0] = 55;
+                niz[2] = 77;
+                niz[1] = 22;
+
+                //niz[3] = 33; nece proci jer nije unutar niza!
+
+                for (int i = 0; i < niz.Length; i++)
+                {
+                    if (niz[i] == null)
+                    {
+                        throw new Exception("Niz sadrži neinicijalizirane vrijednosti na indexu " + i);
+                    }
+                    Console.WriteLine((i + 1) + ": " + niz[i] /*+ niz[5]*/);
+                }
+            }
+            catch(IndexOutOfRangeException ioorex)
+            {
+                Console.WriteLine("Pokušali ste dohvatiti nepostojeći član niza! " + ioorex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/6_2_1_arrayList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2_1_arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = -1;
+            ArrayList arr = new ArrayList();
+
+            Console.WriteLine("Unesi broj. Za kraj unesi <0>");
+
+            while (broj != 0)
+            {
+                broj = int.Parse(Console.ReadLine());
+                arr.Add(broj);
+
+            }
+
+            arr.Add(3.14);
+
+            for (int i = 0; i < arr.Count ; i++)
+
+            {
+                Console.WriteLine(arr[i]);
+
+            }
+
+            foreach (int brojcic in arr)
+            {
+                Console.Write(" " + brojcic);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
