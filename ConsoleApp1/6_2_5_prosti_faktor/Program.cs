5ee8d262d455603f4e84a5cb206fdf099ad9aa3d
staman1702 <staman1702@gmail.com>
2019-03-05 18:15:06 +0100

    prosti_fac

---
+++ ConsoleApp1/6_2_5_prosti_faktor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2_5_prosti_faktor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prirodan broj: ");
+            List<int> prosti = new List<int>();
+            int a = int.Parse(Console.ReadLine());
+
+            int fac = 2;
+
+            while(fac<=a)
+            {
+                if (a % fac == 0)
+                {
+                    prosti.Add(fac);
+                    a = a / fac;
+
+                }
+                else
+                {
+                    fac++;
+                }
+            }
+
+            foreach (int item in prosti)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
