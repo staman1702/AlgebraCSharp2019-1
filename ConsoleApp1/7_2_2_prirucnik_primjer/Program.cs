3e78470b4b888d0b763595d086139458d7edecff
staman1702 <staman1702@gmail.com>
2019-03-05 18:31:16 +0100

    potprogramici_1

---
+++ ConsoleApp1/7_2_2_prirucnik_primjer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_2_2_prirucnik_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozdrav("Pero");
+            Pozdrav("Pero", "Perić");
+            Console.ReadLine();
+
+
+
+        }
+
+        private static void Pozdrav(string ime, string prezime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + " " + prezime);
+        }
+
+        static void Pozdrav(string ime)
+        {
+            Console.WriteLine("Dobar dan, " + ime);
+        }
+    }
+}
---
+++ ConsoleApp1/7_2_potprogrami/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_2_potprogrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+
+            //da smo van Main-a bi morali pisati " Program.HelloWorld(); "
+            //npr:
+            Program.HelloWorld();
+            Console.WriteLine(HelloWorld("Ola"));
+
+            Console.ReadKey();
+        }
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static String HelloWorld(string poz)
+        {
+            return poz + " i tebi korisniče" ;
+        }
+    }
+}
