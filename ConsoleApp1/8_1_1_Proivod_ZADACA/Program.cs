2580afe960379ae3ce5f99ad85b0a7e46a9c30af
staman1702 <staman1702@gmail.com>
2019-03-07 10:27:29 +0100

    ZADACA_Porez

---
+++ ConsoleApp1/8_1_1_Proivod_ZADACA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_1_1_Proivod_ZADACA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proizvod proizvod1 = new Proizvod();
+
+            Console.WriteLine("Unesite naziv proizvoda:");
+            proizvod1.ime = Console.ReadLine();
+
+            Console.WriteLine("Unesite osnovnu cijenu prozvoda:");
+            proizvod1.cijena = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite maržu (%):");
+            proizvod1.marza = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite porez (%):");
+            proizvod1.porez = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Maloprodajna cijena je: {0}", proizvod1.mpc().ToString("#.##"));
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/8_1_1_Proivod_ZADACA/Proizvod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8_1_1_Proivod_ZADACA
+{
+    internal class Proizvod
+    {
+        public string ime = "";
+        public double cijena = 0;
+        public double marza = 0;
+        public double porez = 0;
+        public DateTime datumNastanka;
+
+        public Proizvod()
+        {
+            this.datumNastanka = DateTime.Now;
+        }
+
+        public Proizvod(string v)
+        {
+            this.ime = v;
+        }
+
+        public double mpc()
+        {
+            double mpc = 0;
+            mpc = (double) cijena + ( cijena *  marza / 100 ) + ( cijena *  marza *  porez / 100 / 100);
+            return mpc;
+
+        }
+    }
+}
