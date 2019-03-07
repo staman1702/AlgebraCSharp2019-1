674e881382cc9b9061141150bbc24c1c8ddd79e7
staman1702 <staman1702@gmail.com>
2019-03-07 13:32:03 +0100

    Zadaca_07_03

---
+++ ConsoleApp1/8_1_2_Automobil_ZADACA/Automobil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _8_1_2_Automobil_ZADACA
+{
+    internal class Automobil
+    {
+        public string marka = "";
+        public double ks = 0;
+        public double oscijena = 0;
+        public DateTime datumNastanka;
+
+        public Automobil()
+        {
+            this.datumNastanka = DateTime.Now;
+        }
+
+        public Automobil(string v)
+        {
+            this.marka = v;
+        }
+
+        public double mpc()
+        {
+            double mpc = 0;
+
+            if (ks<50)
+            {
+                mpc = (double) oscijena + (oscijena * 0.05);
+
+                
+            }
+            if (ks>=50 && ks<150)
+            {
+                mpc = (double) oscijena + (oscijena * 0.1);
+
+               
+
+            }
+            else 
+            {
+                mpc = (double) oscijena + (oscijena * 0.15);
+
+            }
+
+            return mpc;
+            
+
+        }
+    }
+}
---
+++ ConsoleApp1/8_1_2_Automobil_ZADACA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_1_2_Automobil_ZADACA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Automobil automobil1 = new Automobil();
+
+            Console.WriteLine("Unesite marku automobila:");
+            automobil1.marka = Console.ReadLine();
+
+            Console.WriteLine("Unesite konjske snage:");
+            automobil1.ks = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite osnovnu cijenu:");
+            automobil1.oscijena = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Cijena sa porezom za vaš automobil je: {0}", automobil1.mpc().ToString("#.##"));
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
