2eff9ba6722fe3a5e3f441956e7bd58f22c9d580
staman1702 <staman1702@gmail.com>
2019-03-08 10:00:35 +0100

    08_03_ZADACA

---
+++ ConsoleApp1/8_1_3_zaposlenik_ZADACA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_1_3_zaposlenik_ZADACA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zaposlenik z1 = new Zaposlenik();
+
+            Console.WriteLine("unesite ime: ");
+            z1.ime = Console.ReadLine();
+
+            Console.WriteLine("unesite prezime: ");
+            z1.prezime = Console.ReadLine();
+
+            Console.WriteLine("unesite oib: ");
+            z1.oib = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("unesite broj bodova: ");
+            z1.bbod = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("unesite vrijednost bodova: ");
+            z1.vbod = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zaposlenik {0} {1}, oib:{2} ima neto placu: {3} hrk.", 
+                                z1.ime, z1.prezime, z1.oib, z1.NetoP());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/8_1_3_zaposlenik_ZADACA/Zaposlenik.cs
@@ -0,0 +1,81 @@
+﻿namespace _8_1_3_zaposlenik_ZADACA
+{
+    internal class Zaposlenik
+    {
+
+        //svojstva
+        public string ime = "";
+        public string prezime= "";
+        public int oib = 0;
+        public double bbod = 0;
+        public double vbod = 0;
+        public double porez = 0;
+        
+   
+        //defaultni konstruktor
+        public Zaposlenik()
+        {
+            
+        }
+
+        //konstruktor sa argumentima (parametrima): ime, prezime
+        public Zaposlenik(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+
+        //konstruktor sa argumentima: ime, prezime, oib
+        public  Zaposlenik( string ime, string prezime, int oib)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.oib = oib;
+        }
+
+
+
+
+
+        //metoda za neto placu
+        public double NetoP()
+        {
+            double netop = 0;
+            netop = (double) this.bbod * this.vbod;
+            return netop;
+        }
+
+        
+        
+        public double Porez()
+        {
+            double porez = 0;
+            
+            if (this.NetoP() < 3000 )
+            {
+                porez = 6 / 100;
+            }
+            else if (this.NetoP() < 6000)
+            {
+                porez = 12 /100;
+            }
+            else
+            {
+                porez = 20 / 100;
+            }
+            return porez;
+
+        }
+
+        //metoda za bruto placu
+        public double BrutoP()
+        {
+            double brutop = 0;
+            brutop = (double)this.NetoP() + (this.NetoP() * this.porez);
+            return brutop;
+
+        }
+
+
+    }
+}
