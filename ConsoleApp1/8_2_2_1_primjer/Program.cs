8fe90a037dad142af0cb4c0015933cf8910d4af2
staman1702 <staman1702@gmail.com>
2019-03-08 18:38:22 +0100

    petak_08_03

---
+++ ConsoleApp1/8_2_2_1_primjer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_2_2_1_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            Console.WriteLine("x={0}",x);
+
+            Povecaj1(x);
+            Console.WriteLine("x={0}",x);
+
+            Povecaj2(ref x);
+            Console.WriteLine("x={0}",x);
+
+            Console.ReadLine();
+        }
+
+        private static void Povecaj1(int a)
+        {
+            a++;
+        }
+
+        private static void Povecaj2(ref int a)
+        {
+            a++;
+        }
+    }
+}
