c3751e656d67c42ae2662eaf4aec7da79fd4cd62
staman1702 <staman1702@gmail.com>
2019-03-10 22:14:05 +0100

    zadaca_vecer

---
+++ ConsoleApp1/ZADACA_11_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZADACA_11_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ucenik ucenik1 = new Ucenik("Mario");
+
+            
+
+        }
+    }
+}
---
+++ ConsoleApp1/ZADACA_11_3/Ucenik.cs
@@ -0,0 +1,45 @@
+﻿namespace ZADACA_11_3
+{
+    internal class Ucenik
+    {
+        public string ime = "";
+        public string prezime = "";
+        string oib = "";
+
+        Ucenik()
+        {
+
+        }
+
+        public string Ime
+        { get => ime;
+          set => ToUpper.FirstCharToUpper(value);
+        }
+
+
+        public string Prezime
+        {
+            get => prezime;
+            set => ToUpper.FirstCharToUpper(value);
+        }
+
+        public string OIB
+        {
+            get { return oib; }
+            set { oib = value; }
+
+        }
+
+        public int racun;
+        public int Racun
+        {
+            get { return racun; }
+            set { racun = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Pozdrav, " + this.Ime + " " + this.Prezime + " " + this.Racun.ToString();
+        }
+    }
+}
