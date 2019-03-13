7427f005bc4f21f0b42b255f8b521c32a3a786b6
staman1702 <staman1702@gmail.com>
2019-03-13 17:26:03 +0100

    eventovi

---
+++ ConsoleApp1/8_5_1_dog_primjer/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_5_1_dog_primjer
+{
+    public class Osoba
+    {
+        public delegate void NaPromjenuImenaDelegat(object sender, EventArgs e);
+
+        public event NaPromjenuImenaDelegat NaPromjenuImena;
+
+
+        private string ime;
+
+        public string Ime
+        {
+            get { return ime; }
+            set
+            {
+                ime = value;
+                if (NaPromjenuImena !=null)
+                {
+                    NaPromjenuImena(this, new EventArgs());
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ ConsoleApp1/8_5_1_dog_primjer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_5_1_dog_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Osoba osoba = new Osoba();
+            osoba.NaPromjenuImena += new
+                Osoba.NaPromjenuImenaDelegat(osoba_NaPromjenuImena);
+            osoba.Ime = "Pero";
+
+            osoba.NaPromjenuImena -= new
+                Osoba.NaPromjenuImenaDelegat(osoba_NaPromjenuImena);
+
+            Console.ReadLine();
+        }
+
+        static void osoba_NaPromjenuImena(object sender, EventArgs e)
+        {
+            Console.WriteLine(
+                "Osoba je promjenila podatke: {0}", ((Osoba)sender).Ime);
+        }
+
+        
+    }
+}
